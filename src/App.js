33c839e4feed430f2059f0228f0e4b44c520eac9
Muhammad Rabi <vonrabi07@yahoo.com>
2022-12-14 00:02:51 +0200

    test01

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css"
 function App() {
   return (
     <main>
-      <h1 className="p-5 text-white text-lg bg-rose-500">hi react</h1>
+      <h1 className="p-5 text-white text-lg bg-rose-500">hi todo</h1>
     </main>
   )
 }
